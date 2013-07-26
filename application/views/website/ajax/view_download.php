9fda3da57cc24febd324de1fe77517e508c735cf
her0satr <her0satr@yahoo.com>
2013-07-25 23:59:40 -0700

    Download support title

--- application/views/website/ajax/view_download.php
+++ application/views/website/ajax/view_download.php
@@ -3,5 +3,9 @@
 ?>
 
 <?php foreach ($post['array_link_source'] as $array) { ?>
+<?php if (is_link($array['link'])) { ?>
 <div><a href="<?php echo $array['link']; ?>"><?php echo $array['base_name']; ?></a></div>
+<?php } else { ?>
+<div><?php echo $array['link'].' '.@$array['base_name']; ?></div>
+<?php } ?>
 <?php } ?>
