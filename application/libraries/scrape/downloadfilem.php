621cd20ef18487828e905f953d874bcffd7bcf97
her0satr <her0satr@yahoo.com>
2014-05-21 15:34:34 +0700

    update

---
+++ application/libraries/scrape/downloadfilem.php
@@ -0,0 +1,145 @@
+<?php
+
+class downloadfilem {
+    function __construct() {
+        $this->CI =& get_instance();
+    }
+    
+	function get_array($scrape) {
+		$curl = new curl();
+		$array_item = array();
+		$content = $curl->get($scrape['link']);
+		$array_post = $this->get_array_clear($content);
+		
+		$array_result = array();
+		foreach ($array_post as $array) {
+			$array['title'] = trim($array['title']);
+			
+			// content already exist
+			$check = $this->CI->Scrape_Content_model->get_by_id(array( 'link_source' => $array['link'] ));
+			if (count($check) > 0) {
+				continue;
+			}
+			
+			$content_html = $this->get_content($array['link']);
+			$desc = $this->get_desc($content_html);
+			$thumbnail = $this->get_image($content_html);
+			
+			// set to array
+			$temp = array();
+			$temp['name'] = $array['title'];
+			$temp['desc'] = $desc;
+			$temp['download'] = $array['link'];
+			$temp['link_source'] = $array['link'];
+			$temp['thumbnail'] = $thumbnail;
+			$temp['category_id'] = $scrape['category_id'];
+			$temp['post_type_id'] = $scrape['post_type_id'];
+			$temp['scrape_master_id'] = $scrape['id'];
+			$temp['scrape_time'] = $this->CI->config->item('current_datetime');
+			$array_result[] = $temp;
+			
+			print_r($array_result); exit;
+			
+			// add limit
+			if (count($array_result) >= 5) {
+				break;
+			}
+		}
+		
+		return $array_result;
+	}
+	
+	function get_array_clear($content) {
+		$array_result = array();
+		
+		// remove start offset
+		$offset = '<!-- begin featured news -->';
+		$pos_first = strpos($content, $offset);
+		$content = substr($content, $pos_first, strlen($content) - $pos_first);
+		
+		// remove end offset
+		$offset = '<!-- end featured news -->';
+		$pos_end = strpos($content, $offset);
+		$content = substr($content, 0, $pos_end);
+		
+		preg_match_all('/<h3><a href="([^"]+)">FILM ([^<]+)<\/a>/i', $content, $match);
+		foreach ($match[0] as $key => $value) {
+			if (!empty($match[1][$key]) && $match[2][$key]) {
+				$array_temp = array( 'link' => $match[1][$key], 'title' => trim(ucwords(strtolower($match[2][$key]))) );
+				$array_result[] = $array_temp;
+			}
+		}
+		
+		return $array_result;
+	}
+	
+	function get_content($link_source) {
+		$curl = new curl();
+		$content = $curl->get($link_source);
+		$content = preg_replace('/[^\x20-\x7E|\x0A]/i', '', $content);
+		
+		// remove start offset
+		$offset = '<!-- begin post -->';
+		$pos_first = strpos($content, $offset);
+		$content = substr($content, $pos_first, strlen($content) - $pos_first);
+		
+		// remove end offset
+		$offset = '<!-- end post -->';
+		$pos_end = strpos($content, $offset);
+		$content = substr($content, 0, $pos_end);
+		
+		// remove wording
+		$content = preg_replace('/<a href="[^"]+">\s*(<b>)*Download\/internet[^<]+(<\/b>)*<\/a>/i', '', $content);
+		
+		// remove additional
+		$offset = 'Download MF.php';
+		$pos_end = strpos($content, $offset);
+		if ($pos_end) {
+			$content = substr($content, 0, $pos_end);
+		}
+		
+		return $content;
+	}
+	
+	function get_desc($content) {
+		// remove start offset
+		$offset = '<img';
+		$pos_first = strpos($content, $offset);
+		$content = substr($content, $pos_first, strlen($content) - $pos_first);
+		
+		// make nice format
+		$content = trim(strip_tags($content));
+		$array_temp = explode("\n", $content);
+		foreach ($array_temp as $key => $line) {
+			if (empty($line)) {
+				unset($array_temp[$key]);
+			}
+		}
+		$result = implode('<br />', $array_temp);
+		
+		// endfix
+		if (!empty($result)) {
+			$result .= '<br /><br /><div>Sumber : Download Filem</div>';
+		}
+		
+		return $result;
+	}
+	
+	function get_image($content) {
+		$content = preg_replace('/ (style|alt|border|height|width)="[^"]*"/i', '', $content);
+		
+		// get link image
+		preg_match('/src="(http:\/\/www.downloadfilem.com\/wp-content\/uploads\/\d+\/\d+[^"]+)"/i', $content, $match);
+		$result = (isset($match[1]) && !empty($match[1])) ? $match[1] : '';
+		
+		// write image
+		if (!empty($result)) {
+			$download_result = download_image($result);
+			if ($download_result['status']) {
+				$result = $download_result['dir_image_path'];
+			}
+		}
+		
+		return $result;
+	}
+}
